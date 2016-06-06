3973b010952b6b6a50636410ba8fcb03e269d1bd
ponhuahinCA <lovehuahinlove@windowslive.com>
2016-06-06 15:18:03 +0700

    ออกแบบปุ่ม SignIN/UPแล้ว

---
+++ app/src/main/java/ca/mogkolpon/tipbru/SignUPActivity.java
@@ -0,0 +1,13 @@
+package ca.mogkolpon.tipbru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
