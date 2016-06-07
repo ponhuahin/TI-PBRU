950d61fc63a91d0c5dd1d4bc647fc965e73a30d2
ponhuahinCA <lovehuahinlove@windowslive.com>
2016-06-07 16:00:11 +0700

    Delete

--- app/src/main/java/ca/mogkolpon/tipbru/MainActivity.java
+++ app/src/main/java/ca/mogkolpon/tipbru/MainActivity.java
@@ -1,6 +1,7 @@
 package ca.mogkolpon.tipbru;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,8 +21,20 @@ public class MainActivity extends AppCompatActivity {
         //Test Add New User  ทดสอบ
         //myManage.addNewUser("123","name","sur","user","pass");
 
+        //Delete All SQLite
+        deleteAllSQLite();
+
 
     } // Main Method
+
+    private void deleteAllSQLite() { //ลบฐานข้อมูลทั่งหมด
+
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        sqLiteDatabase.delete(MyManage.user_table, null, null);
+
+
+    } //deleteAllSQLite
 
     public void clickSignUpMain(View view) {  // View view คือ ให้ฝัง หน้าออกแบบเห็น
         startActivity(new Intent(MainActivity.this, SignUPActivity.class));
