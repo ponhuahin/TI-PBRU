e8e4d9e7008ba0fb6223d8aedd6a1ef025736f02
ponhuahinCA <lovehuahinlove@windowslive.com>
2016-06-07 11:02:43 +0700

    Check เรียบร้อยแล้ว

---
+++ app/src/main/java/ca/mogkolpon/tipbru/MyAlert.java
@@ -0,0 +1,34 @@
+package ca.mogkolpon.tipbru;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by lap324-04 on 6/7/16 AD.
+ */
+public class MyAlert {
+
+    public void myDialog(Context context, String strTitle, String strMessage) { //context คือการส่ง คลาส ถึงคลาส
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);  //ถ้าป็อปอัพขึ้นไม่ให้กดกลับได้
+        builder.setIcon(R.drawable.nobita48);  //ใส่ภาพที่ต้องการแจ้งเตือน
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();  //เมื่อกด ok ป็อปอัพจะหายไป
+
+           }
+        });
+
+        builder.show();
+
+
+    }
+
+
+
+
+} // Main Class
--- app/src/main/java/ca/mogkolpon/tipbru/SignUPActivity.java
+++ app/src/main/java/ca/mogkolpon/tipbru/SignUPActivity.java
@@ -2,13 +2,59 @@ package ca.mogkolpon.tipbru;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUPActivity extends AppCompatActivity {
+
+    // Explicit ประกาศตัวแปร
+    private EditText nameEditText, surnameEditText, userEditText, passwordEditText;
+    private String nameString, surnameString, userString, passwoedString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Bind Widge
+        nameEditText = (EditText) findViewById(R.id.editText);
+        surnameEditText= (EditText) findViewById(R.id.editText2);
+        userEditText= (EditText) findViewById(R.id.editText3);
+        passwordEditText= (EditText) findViewById(R.id.editText4);
+
     } // Main Method
+
+    public void clickSignUpSign(View view) {
+
+
+        nameString = nameEditText.getText().toString().trim();  //trim คือตัดช่องว่างออก
+        surnameString=surnameEditText.getText().toString().trim();
+        userString=userEditText.getText().toString().trim();
+        passwoedString=passwordEditText.getText().toString().trim();
+
+        //Chech Space เซ็คช่องว่าง
+        if (checkSpace()) {
+            //True
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this,"มีช่วงว่าง", "กรุณากรอกทุกช่วง");
+
+        } else {
+            //False
+
+
+        }
+
+    } // clinkSign คลิกปุ่ม SignUp
+
+    private boolean checkSpace() {
+
+        boolean result = true;
+
+        result = nameString.equals("") || surnameString.equals("") ||
+                userString.equals("") || passwoedString.equals("");
+
+        return result;
+    }
+
 
 } // Mani Class
